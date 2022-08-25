1fcb2304fb13e0d3768663db449a403d6a059042
Mark Struckmann <mark.struckmann@yahoo.com>
2022-08-25 17:55:21 -0500

    3 execises

--- src/org/launchcode/java/demos/lsn2controlflowandcollections/Chapter3Exercise.java
+++ src/org/launchcode/java/demos/lsn2controlflowandcollections/Chapter3Exercise.java
@@ -2,9 +2,18 @@ package org.launchcode.java.demos.lsn2controlflowandcollections;
 
 public class Chapter3Exercise {
     public static void main(String[] args) {
-        int nums[] = {1, 1, 2, 3, 5, 8};
-        for (int i : nums) {
-            System.out.println(i);
+        int[] myArray = {1, 1, 2, 3, 5, 8};
+
+        for (int integer: myArray){
+            System.out.println(integer);
+        }
+
+        for(int i = 0; i < myArray.length; i++){
+            if(myArray[i]%2 == 0){
+                System.out.println("not odd condition: " + myArray[i]);
+                continue;
+            }
+            System.out.println(myArray[i]);
         }
     }
 
